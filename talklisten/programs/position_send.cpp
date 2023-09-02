ff776cfad29792fe26e648445691256e759dbf6b
Vignesh Aravindh B <118040393+codechiefVignesh@users.noreply.github.com>
2023-09-02 21:57:31 +0530

    Update position_send.cpp

--- talklisten/programs/position_send.cpp
+++ talklisten/programs/position_send.cpp
@@ -1,12 +1,16 @@
 #include"ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
 #include"talklisten/Position.h"
 bool position(talklisten::Position::Request &req, talklisten::Position::Response &res)
 {
+    res.posx = req.posx;
+    res.posy = req.posy;
     res.chr = req.ch;
     if(res.chr=='w')
     {
-        res.posx = req.posx+5.0;
-        res.posy = req.posy+5.0;
+        res.posx = res.posx+5.0;
+        res.posy = res.posy+5.0;
         res.dir = "north";
     }
     else if(res.chr=='d')
@@ -17,19 +21,19 @@ bool position(talklisten::Position::Request &req, talklisten::Position::Response
     {
         res.dir = "west";
     }
-    else
+    else 
     {
         res.dir = "south";
     }
-    ROS_INFO("request: key = %c",(char)req.ch);
-    ROS_INFO("moving the rover: x:[%f], y:[%f]",res.posx,res.posy);
+    ROS_INFO("request: x=%ld y=%ld key = %c",(long int)req.posx,(long int)req.posy, (char)req.ch);
+    ROS_INFO("moving the rover: x:[%ld], y:[%ld], direction:[%s]",res.posx,res.posy,res.dir.c_str());
     return true;
 }   
 int main(int argc, char **argv)
 {
     ros::init(argc,argv,"position_send");
     ros::NodeHandle n;
-    ros::ServiceServer service = n.advertiseService("position_send",position);
+    ros::ServiceServer service = n.advertiseService("position",position);
     ROS_INFO("Ready to move the rover");
     ros::spin();
     return 0;

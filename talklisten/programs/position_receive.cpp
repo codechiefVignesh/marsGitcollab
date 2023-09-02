d8346be85fdaa4c8ccaa9ddb1980b760862a86a3
Vignesh Aravindh B <118040393+codechiefVignesh@users.noreply.github.com>
2023-09-02 21:57:00 +0530

    Update position_receive.cpp

--- talklisten/programs/position_receive.cpp
+++ talklisten/programs/position_receive.cpp
@@ -1,27 +1,31 @@
 #include "ros/ros.h"
 #include "talklisten/Position.h"
+#include "std_msgs/String.h"
+#include <sstream>
 #include <cstdlib>
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "position_receive");
-  if (argc != 5) 
+  if (argc < 4) 
   {
-    ROS_INFO("usage: position_receive X Y");
+    ROS_INFO("usage: position_receive X Y and Direction");
     return 1;
   }
 
   ros::NodeHandle n;
   ros::ServiceClient client = n.serviceClient<talklisten::Position>("position");
   talklisten::Position srv;
-  srv.request.ch = atoll(argv[1]);
+  srv.request.posx = atoll(argv[1]);
+  srv.request.posy = atoll(argv[2]);
+  srv.request.ch = argv[3][0];
   if (client.call(srv))
   {
-    ROS_INFO("moving the rover: x:[%f], y:[%f])",srv.response.posx,srv.response.posy);
+    ROS_INFO("moving the rover: x:[%ld], y:[%ld],dir:[%s])",srv.response.posx,srv.response.posy,srv.response.dir.c_str());
   }
   else
   {
-    ROS_ERROR("Failed to call service position_");
+    ROS_ERROR("Failed to call service position");
     return 1;
   }
 
